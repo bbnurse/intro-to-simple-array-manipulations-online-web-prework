d5b21f7c1a7a017d4776b7c541030bb3fae15f68
blanca bell <b.p.bell15@gmail.com>
2019-01-01 22:57:53 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,3 +10,9 @@ def using_unshift(new_neighborhood, neighborhoods_in_northwest_brooklyn)
   new_neighborhood = "Brooklyn Heights"
   neighborhoods_in_northwest_brooklyn.unshift(new_neighborhood)
 end 
+
+
+def using_pop
+  great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
+  deleted_string = "Losing My Religion"
+  great_hits_of_the_nineties.pop(deleted_string)
