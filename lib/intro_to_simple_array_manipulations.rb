cd4c9950a77c9dab213667b0ef6019cae0ff2815
blanca bell <b.p.bell15@gmail.com>
2019-01-01 22:58:28 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -14,6 +14,5 @@ end
 
 def using_pop
   great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
-  deleted_string = "Losing My Religion"
-  great_hits_of_the_nineties.pop(deleted_string)
+  great_hits_of_the_nineties.pop
 end
